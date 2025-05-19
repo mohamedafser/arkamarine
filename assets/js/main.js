3802aa1bd4ace1a49c1f70f5ee7d97c7a5e1b148
Mohamed <mohamedafser2203@gmail.com>
2025-05-19 11:40:41 +0530

    revert header footer code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
+const url = new URL(window.location.href);
+// console.log(url.pathname);
+const path = url.pathname.replace(/\//g, "");
+
+fetch("../../header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+    $("#menu-items li")
+      .map(function () {
+        const id = $(this).data("id");
+        if (id === path) {
+          $(this).addClass("active");
+        }
+      })
+      .get();
+  });
+
+fetch("../../footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+fetch("../../maintanance.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("maintenance").innerHTML = data;
+  });
+
 (function ($) {
   "use strict";
 
